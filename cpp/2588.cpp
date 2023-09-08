ab60a5b263c7e9482cd48b8a5ed89e07797e1355
yuedward0103 <edward0103.yu@gmail.com>
2023-09-08 06:18:22 +0000

    230908:solve..?

---
+++ cpp/2588.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+/*
+	int a=0;
+	char b[3] = NULL;
+	int b0 = 0;
+	int a3=0, a4=0, a5=0;
+	cin >> a;
+	cin >> b;
+	a3 = a * b[2];
+	a4 = a * b[1];
+	a5 = a * b[0];
+	b0 = a * (int)b[0]*100 + (int)b[1]*10 + (int)b[2];
+	cout >> b0;
+	//cout >> a3 >> endl >> a4 >> endl >> a5 >> endl >> a*b0 >> endl;
+*/
+	
+	return 0;
+}
